2f7aa8fcfaf68738b66b80d0a98e7b6da69ce5c8
Марина Колединцева <marinkoled@yandex.ru>
2016-07-05 17:52:11 +0600

    добавление функций Выход, Сохранить

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include "ReportDialog.h"
+//#include "ReportDialog.h"
+
+#include <QAction>
+#include <QMenu>
+#include <QDebug>
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -13,6 +18,26 @@ MainWindow::MainWindow(QWidget *parent) :
 //        ReportDialog rd;
 //        rd.exec();
 //    });
+
+    connect (ui->action_Save, &QAction::triggered, this, [this](){
+        QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), "", tr("Text Files (*.txt)"));
+
+        QFile file(fileName);
+        file.open(QIODevice::WriteOnly);
+        QTextStream stream(&file);
+        stream << ui->plainTextEdit->toPlainText();
+        stream.flush();
+        file.close();
+    });
+
+    connect (ui ->action_Print, &QAction::triggered, this, [this]() {
+        qDebug()<<"Print";
+
+    });
+
+    connect(ui->action_Exit, &QAction::triggered, this, [this] (){
+        QMainWindow::close();
+    });
 }
 
 MainWindow::~MainWindow()
